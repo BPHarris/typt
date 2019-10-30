1e86d5c730bc2fa8050ea0cfc29b881b427f6a2c
bpharris <brandon.harris1@btinternet.com>
2019-10-30 22:35:29 +0000

    Initial files

---
+++ Typt/TyptListener.py
@@ -0,0 +1,19 @@
+# Generated from Typt.g4 by ANTLR 4.7.2
+from antlr4 import *
+if __name__ is not None and "." in __name__:
+    from .TyptParser import TyptParser
+else:
+    from TyptParser import TyptParser
+
+# This class defines a complete listener for a parse tree produced by TyptParser.
+class TyptListener(ParseTreeListener):
+
+    # Enter a parse tree produced by TyptParser#program.
+    def enterProgram(self, ctx:TyptParser.ProgramContext):
+        pass
+
+    # Exit a parse tree produced by TyptParser#program.
+    def exitProgram(self, ctx:TyptParser.ProgramContext):
+        pass
+
+
---
+++ Typt/TyptParser.py
@@ -0,0 +1,89 @@
+# Generated from Typt.g4 by ANTLR 4.7.2
+# encoding: utf-8
+from antlr4 import *
+from io import StringIO
+from typing.io import TextIO
+import sys
+
+
+def serializedATN():
+    with StringIO() as buf:
+        buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3\3")
+        buf.write("\7\4\2\t\2\3\2\3\2\3\2\2\2\3\2\2\2\2\5\2\4\3\2\2\2\4\5")
+        buf.write("\7\2\2\3\5\3\3\2\2\2\2")
+        return buf.getvalue()
+
+
+class TyptParser ( Parser ):
+
+    grammarFileName = "Typt.g4"
+
+    atn = ATNDeserializer().deserialize(serializedATN())
+
+    decisionsToDFA = [ DFA(ds, i) for i, ds in enumerate(atn.decisionToState) ]
+
+    sharedContextCache = PredictionContextCache()
+
+    literalNames = [  ]
+
+    symbolicNames = [ "<INVALID>", "DIGIT" ]
+
+    RULE_program = 0
+
+    ruleNames =  [ "program" ]
+
+    EOF = Token.EOF
+    DIGIT=1
+
+    def __init__(self, input:TokenStream, output:TextIO = sys.stdout):
+        super().__init__(input, output)
+        self.checkVersion("4.7.2")
+        self._interp = ParserATNSimulator(self, self.atn, self.decisionsToDFA, self.sharedContextCache)
+        self._predicates = None
+
+
+
+
+    class ProgramContext(ParserRuleContext):
+
+        def __init__(self, parser, parent:ParserRuleContext=None, invokingState:int=-1):
+            super().__init__(parent, invokingState)
+            self.parser = parser
+
+        def EOF(self):
+            return self.getToken(TyptParser.EOF, 0)
+
+        def getRuleIndex(self):
+            return TyptParser.RULE_program
+
+        def enterRule(self, listener:ParseTreeListener):
+            if hasattr( listener, "enterProgram" ):
+                listener.enterProgram(self)
+
+        def exitRule(self, listener:ParseTreeListener):
+            if hasattr( listener, "exitProgram" ):
+                listener.exitProgram(self)
+
+
+
+
+    def program(self):
+
+        localctx = TyptParser.ProgramContext(self, self._ctx, self.state)
+        self.enterRule(localctx, 0, self.RULE_program)
+        try:
+            self.enterOuterAlt(localctx, 1)
+            self.state = 2
+            self.match(TyptParser.EOF)
+        except RecognitionException as re:
+            localctx.exception = re
+            self._errHandler.reportError(self, re)
+            self._errHandler.recover(self, re)
+        finally:
+            self.exitRule()
+        return localctx
+
+
+
+
+
