ac22bb19396bfd4194f237ecf9153e0f9237aade
bpharris <brandon.harris1@btinternet.com>
2019-10-30 22:51:45 +0000

    Initial unittest file

---
+++ test.py
@@ -0,0 +1,13 @@
+"""test.py - unittests for Typt.py file."""
+
+import unittest
+
+
+class TestFileHandling(unittest.TestCase):
+    """Class for file handling test cases."""
+
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
