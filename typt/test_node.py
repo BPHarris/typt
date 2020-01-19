366112a2b5bdb14e2f3a46d0e1f94cd9c2b9a0c3
bpharris <brandon.harris1@btinternet.com>
2020-01-19 23:58:30 +0000

    Added TestNode

---
+++ typt/test_node.py
@@ -0,0 +1,11 @@
+"""test_node.py - holds the AST node for a test statement."""
+
+from typt.node import Node
+
+
+class TestNode(Node):
+    """TestNode AST node."""
+
+    def __init__(self, depth: int = 0):
+        """Call super initialiser."""
+        super().__init__()
