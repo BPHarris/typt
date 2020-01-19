c1171ed34af6b32ae12b98ee86a3cc7fce2be663
bpharris <brandon.harris1@btinternet.com>
2020-01-19 21:51:55 +0000

    Created UsingNode

---
+++ typt/using_node.py
@@ -0,0 +1,22 @@
+"""using_node.py - holds the AST node for the program rule."""
+
+from typt.node import Node
+
+
+class UsingNode(Node):
+    """Using AST node.
+
+    Attributes:
+        function_signature_list (list[UsingNode])   : The function signatures
+        library_name            (str)               : The library name
+        library_alias           (str)               : The library alias
+
+    """
+
+    def __init__(self, library_name: str = '', library_alias: str = ''):
+        """Initialise using_list and stmt_list."""
+        self.function_signature_list = list()
+        self.library_name = library_name
+        self.library_alias = library_alias
+
+        super().__init__()
