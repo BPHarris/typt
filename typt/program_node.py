6d2240e0b13b02f0e8f29b0234ae80d60ab37d8b
bpharris <brandon.harris1@btinternet.com>
2020-01-19 21:15:56 +0000

    Created ProgramNode for AST

---
+++ typt/program_node.py
@@ -0,0 +1,20 @@
+"""program_node.py - holds the AST node for the program rule."""
+
+from typt.node import Node
+
+
+class ProgramNode(Node):
+    """Program AST node.
+
+    Attributes:
+        using_list  (list[UsingNode])   : The program's using-declarations
+        stmt_list   (list[StmtNode])    : The program's statements
+
+    """
+
+    def __init__(self):
+        """Initialise using_list and stmt_list."""
+        self.using_list = list()
+        self.stmt_list = list()
+
+        super().__init__()
--- typt/visitor.py
+++ typt/visitor.py
@@ -3,7 +3,8 @@
 from antlr.TyptParser import TyptParser
 from antlr.TyptVisitor import TyptVisitor
 
-import typt.node
+from typt.node import Node
+from typt.program_node import ProgramNode
 
 
 class Typt(TyptVisitor):
@@ -16,11 +17,30 @@ class Typt(TyptVisitor):
         super().__init__()
 
     def visitProgram(self, ctx: TyptParser.ProgramContext):
-        """Visit `program' rule."""
-        print('porgram')
-        return self.visitChildren(ctx)
+        """Visit `program' rule.
+
+        Args:
+            ctx (ProgramContext) : The program context
+
+        Get the child using-declarations and statements.
+
+        program => using* stmt* EOF
+
+        """
+
+        self.program = ProgramNode()
+
+        # Add using-declarations to program
+        for using_ctx in ctx.using():
+            using = self.visitUsing(using_ctx)
+            self.program.using_list += [using]
+
+        # Add stmts to program
+        for stmt_ctx in ctx.stmt():
+            stmt = self.visitStmt(stmt_ctx)
+
+        return self.program
     
     def visitStmt(self, ctx: TyptParser.StmtContext):
         """Visit `stmt' rule."""
-        print('stmt')
         return self.visitChildren(ctx)
