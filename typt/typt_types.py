5863efc1a2c2b397e5d73289cacae4066da629e6
bpharris <brandon.harris1@btinternet.com>
2020-01-19 17:48:31 +0000

    Added types

---
+++ typt/typt_types.py
@@ -0,0 +1,73 @@
+"""types.py - classes representing typt types."""
+
+# Todo list
+#   TODO: Object base type
+#   TODO: Object type
+#   TODO: Function type
+#   TODO: List type
+#   TODO: Tuple type
+#   TODO: Set type
+#   TODO: Dict type
+#
+#   TODO: compare_types: add typing rules
+
+
+
+def compare_types(a: Type, b: Type) -> bool:
+    """Compare the given types A and B.
+
+    Return true if the two given types are sematically equivalent int the
+    typing rules for the Typt language. Return false otherwise.
+
+    Args:
+        a   (Type) : First given type.
+        b   (Type) : Second given type.
+    """
+    pass
+
+
+class Type:
+    """A base class for Typt types."""
+
+    def __init__(self):
+        """Set base variables."""
+        pass
+
+    def __eq__(self: Type, other: Type) -> bool:
+        """Compare self to the given type."""
+        return compare_types(self, other)
+
+
+class NoneType(Type):
+    """Class for Typt::None type."""
+
+    def __init__(self):
+        super().__init__()
+
+
+class BoolType(Type):
+    """Class for Typt::Bool type."""
+
+    def __init__(self):
+        super().__init__()
+
+
+class IntType(Type):
+    """Class for Typt::Int type."""
+
+    def __init__(self):
+        super().__init__()
+
+
+class FloatType(Type):
+    """Class for Typt::Float type."""
+
+    def __init__(self):
+        super().__init__()
+
+
+class StringType(Type):
+    """Class for Typt::String type."""
+
+    def __init__(self):
+        super().__init__()
