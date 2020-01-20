e939bbaed2a2af04503b27f67ceca73936be611e
bpharris <brandon.harris1@btinternet.com>
2020-01-20 00:09:59 +0000

    Created AssignmentExprStmtNode

---
+++ typt/assignment_expr_stmt_node.py
@@ -0,0 +1,16 @@
+"""assignment_expr_stmt_node.py - holds the AST node for an assignment expression statement."""
+
+from typt.stmt_node import StmtNode
+from typt.test_node import TestNode
+
+
+class AssignmentExprStmtNode(StmtNode):
+    """AssignmentExprStmtNode AST node."""
+
+    def __init__(self, lhs: TestNode, assignment_type: str, rhs: TestNode, depth: int = 0):
+        """Set the lhs, rhs, and the type of assignment (=, +=, etc.)."""
+        self.lhs = lhs
+        self.assignment_type = assignment_type
+        self.rhs = rhs
+
+        super().__init__()
