c1831cbda9763ce868625aeec9a8f95fd849a778
bpharris <brandon.harris1@btinternet.com>
2020-01-20 01:10:46 +0000

    Fixed return value being needed (now optional)

--- typt/return_stmt_node.py
+++ typt/return_stmt_node.py
@@ -1,12 +1,13 @@
 """return_stmt_node.py - holds the AST node for return statements."""
 
 from typt.stmt_node import StmtNode
+from typt.test_node import TestNode
 
 
 class ReturnStmtNode(StmtNode):
     """ReturnStmt AST node."""
 
-    def __init__(self, return_value: TestNode, depth=0):
+    def __init__(self, return_value: TestNode = None, depth=0):
         """Set return_value."""
         self.return_value = return_value
 
