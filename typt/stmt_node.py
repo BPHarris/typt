f7be6af8e716f0ec3822fcea16146809d08fedb9
bpharris <brandon.harris1@btinternet.com>
2020-01-20 00:01:55 +0000

    Removed unused init parameters

--- typt/stmt_node.py
+++ typt/stmt_node.py
@@ -6,6 +6,6 @@ from typt.node import Node
 class StmtNode(Node):
     """StmtNode AST node."""
 
-    def __init__(self, name: str, return_type: str, depth: int = 0):
+    def __init__(self, depth: int = 0):
         """Call super initialiser."""
         super().__init__()
