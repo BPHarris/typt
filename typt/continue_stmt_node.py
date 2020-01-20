5cdc23c93e4f72d2c290b813c128c9e33bd4ba8a
bpharris <brandon.harris1@btinternet.com>
2020-01-20 00:52:27 +0000

    Created ContinueStmtNode

---
+++ typt/continue_stmt_node.py
@@ -0,0 +1,9 @@
+"""continue_stmt_node.py - holds the AST node for continue statements."""
+
+from typt.stmt_node import StmtNode
+
+
+class ContinueStmtNode(StmtNode):
+    """Placeholder."""
+
+    pass
