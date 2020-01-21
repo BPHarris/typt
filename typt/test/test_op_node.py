34c558162762cb15f756db7f74e2e9ccbff5fc13
Brandon Harris <b.harris.4@warwick.ac.uk>
2020-01-21 23:16:45 +0000

    Added type annotation for list member variable

--- typt/test/test_op_node.py
+++ typt/test/test_op_node.py
@@ -6,6 +6,8 @@ A test operation is one of or_test, and_test, or not_test in Typt.g4.
 
 from typt.test_node import TestNode
 
+from typing import List
+
 
 class TestOpNode(TestNode):
     """TestOpNode AST node."""
@@ -13,4 +15,4 @@ class TestOpNode(TestNode):
     def __init__(self, operator: str, lhs: TestNode):
         """Create initial operand list as just the lhs operand."""
         self.operator = operator
-        self.operands = list([lhs])
+        self.operands = list([lhs])     # type: List[TestNode]
