f590be854c60c59b72c105de71f21dc1206c1100
Brandon Harris <b.harris.4@warwick.ac.uk>
2020-01-21 22:12:55 +0000

    TestOpNode stores operator

--- typt/test/test_op_node.py
+++ typt/test/test_op_node.py
@@ -10,6 +10,7 @@ from typt.test_node import TestNode
 class TestOpNode(TestNode):
     """TestOpNode AST node."""
 
-    def __init__(self, lhs: TestNode):
+    def __init__(self, operator: str, lhs: TestNode):
         """Create initial operand list as just the lhs operand."""
+        self.operator = operator
         self.operands = list([lhs])
