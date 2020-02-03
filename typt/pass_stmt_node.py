d5647d3b59c42d54cbd5b7ebcd4455d02418e550
Brandon Harris <b.harris.4@warwick.ac.uk>
2020-02-03 18:44:14 +0000

    Added type checking for pass

--- typt/pass_stmt_node.py
+++ typt/pass_stmt_node.py
@@ -2,8 +2,14 @@
 
 from typt.stmt_node import StmtNode
 
+from typt.typt_types import Type
+from typt.environment import Environment
+
 
 class PassStmtNode(StmtNode):
     """Nothing to be implemented."""
 
-    pass
+    def check_type(self, environment: Environment) -> Type:
+        """Check the type of the pass statement."""
+        # RULE Pass statement is always well-formed
+        return Type()
