06d15c55679c61ceae19694de94957d072e8a5ff
Brandon Harris <b.harris.4@warwick.ac.uk>
2020-02-04 16:39:11 +0000

    Added codegen and check_type

--- typt/test/var_ref_node.py
+++ typt/test/var_ref_node.py
@@ -2,12 +2,24 @@
 
 from typt.test.atom_node import AtomNode
 
+from typt.codegen import indent
+from typt.typt_types import Type
+from typt.environment import Environment
+
 
 class VarRefNode(AtomNode):
     """VarRefNode AST node."""
 
-    def __init__(self, var_name: str, depth: int = 0):
+    def __init__(self, var_name: str, *args, **kwargs):
         """Set the variable name (var_name)."""
         self.var_name = var_name
 
-        super().__init__(depth=depth)
+        super().__init__(*args, **kwargs)
+
+    def check_type(self, environment: Environment) -> Type:
+        """Return the type of the variable being referenced."""
+        return environment[self.var_name]
+
+    def codegen(self, indentation_level: int = 0) -> str:
+        """Return code of the variable refence, in Python3 form."""
+        return f'{indent(indentation_level)}{self.var_name}'
