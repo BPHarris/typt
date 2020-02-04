831fac3231808493d8d2f02bf6ae4a3e93b691d4
Brandon Harris <b.harris.4@warwick.ac.uk>
2020-02-04 16:12:53 +0000

    Added check_type and codegen for if-stmt

--- typt/if_stmt_node.py
+++ typt/if_stmt_node.py
@@ -1,9 +1,11 @@
-"""if_stmt_node.py - holds the AST node for if statements."""
+"""if_stmt_node.py -- holds the AST node for if statements."""
 
 from typt.stmt_node import StmtNode
 from typt.suite_node import SuiteNode
 
-from typt.typt_types import TestSuitePair
+from typt.codegen import indent
+from typt.environment import Environment
+from typt.typt_types import TestSuitePair, Type, InvalidType, is_invalid_type
 
 from typing import Iterable
 
@@ -11,10 +13,58 @@ from typing import Iterable
 class IfStmtNode(StmtNode):
     """If statement AST node."""
 
-    def __init__(self, if_branch: TestSuitePair, depth: int = 0):
+    def __init__(self, if_branch: TestSuitePair):
         """Set initial values."""
         self.if_branch = if_branch      # type: TestSuitePair
         self.elif_branches = list()     # type: Iterable[TestSuitePair]
         self.else_branch = None         # type: SuiteNode
 
-        super().__init__(depth=depth)
+        super().__init__()
+
+    def check_type(self, environment: Environment) -> Type:
+        """Perform type checking on if-stmt."""
+        # RULE Each test must be valid
+        # RULE Each test must be implicitly coercable to a BoolType
+        # RULE Each suite must be valid
+
+        test_types = []
+        test_types_invalid = []
+
+        # Check RULE 1
+        for branch in [self.if_branch] + self.elif_branches:
+            test_types += branch.test.check_type(environment)
+
+        # Check RULE 2
+        # All valid types can be coerced to a Boolean
+        # NOTE Does not log_type_error as this is handled by RULE 1
+        for test_type in test_types:
+            test_types_invalid += is_invalid_type(test_type)
+
+        # Check RULE 3
+        self.if_branch.suite.check_type(environment)
+        for branch in self.elif_branches:
+            branch.suite.check_type(environment)
+        self.else_branch.check_type(environment)
+
+        return InvalidType() if any(test_types_invalid) else Type()
+
+    def codegen(self, indentation_level: int = 0) -> str:
+        """Return equivalent Python3 code."""
+        indentation = indent(indentation_level)
+
+        # If-branch
+        if_branch = f'{indentation}if {self.if_branch.test.codegen()}:\n'
+        if_branch += f'{self.if_branch.suite.codegen(indentation_level + 1)}'
+
+        # Elif-branches
+        elif_branches = list()
+        for branch in self.elif_branches:
+            elif_branches = f'{indentation}elif {branch.test.codegen()}:\n'
+            elif_branches += f'{branch.suite.codegen(indentation_level + 1)}'
+
+        # Else codegen
+        else_branch = ''
+        if self.else_branch:
+            else_branch = f'else:\n{self.else_branch.codegen(indentation_level + 1)}'
+
+        return f'{if_branch}{str().join(elif_branches)}{else_branch}'
