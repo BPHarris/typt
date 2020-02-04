08db033ed2e81bab99b8c07cd93253d9fadf3d25
Brandon Harris <b.harris.4@warwick.ac.uk>
2020-02-04 14:28:26 +0000

    Added codegen and type checking to continue stmt

--- typt/continue_stmt_node.py
+++ typt/continue_stmt_node.py
@@ -1,9 +1,26 @@
-"""continue_stmt_node.py - holds the AST node for continue statements."""
+"""continue_stmt_node.py -- holds the AST node for continue statements."""
 
 from typt.stmt_node import StmtNode
 
+from typt.codegen import indent
+from typt.typt_types import Type, log_type_error
+from typt.environment import Environment
+
 
 class ContinueStmtNode(StmtNode):
-    """Placeholder."""
+    """Class for 'continue' statement AST node."""
 
-    pass
+    def check_type(self, environment: Environment) -> Type:
+        """Perform type checking on this break statement."""
+        # RULE Continue stmt can only occur inside a loop (for/while)
+        if environment.in_scope('for') or environment.in_scope('while'):
+            return log_type_error(
+                'continue not inside loop', environment.filename, self.meta
+            )
+
+        return Type()
+
+    def codegen(self, indentation_level: int = 0) -> str:
+        """Return the Python3 equivelent to a continue stmt."""
+        return '{indent}continue'.format(indent=indent(indentation_level))
+
