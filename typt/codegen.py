e10ac3784d0b05fd75f9a96fa51c408d4d122002
Brandon Harris <b.harris.4@warwick.ac.uk>
2020-02-04 13:58:10 +0000

    Made indent_str private (to prevent exposure on import)

--- typt/codegen.py
+++ typt/codegen.py
@@ -1,9 +1,9 @@
 """codegen.py -- stores util functions for codegen."""
 
 # The string representing a single indent
-indent_str = '    '     # type: str
+_indent_str = '    '     # type: str
 
 
 def indent(indentation_level: int) -> str:
     """Return the string representing the current indentation level."""
-    return indent_str * indentation_level
+    return _indent_str * indentation_level
