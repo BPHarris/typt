3daac8f48483009d6bfeabdfbb843df9da9e819e
Brandon Harris <b.harris.4@warwick.ac.uk>
2020-02-04 14:20:10 +0000

    Added type check and codegen for break stmt

--- typt/break_stmt_node.py
+++ typt/break_stmt_node.py
@@ -2,8 +2,24 @@
 
 from typt.stmt_node import StmtNode
 
+from typt.codegen import indent
+from typt.typt_types import Type, log_type_error
+from typt.environment import Environment
+
 
 class BreakStmtNode(StmtNode):
-    """Placeholder."""
+    """Class 'break' statement AST node."""
 
-    pass
+    def check_type(self, environment: Environment) -> Type:
+        """Perform type checking on this break statement."""
+        # RULE Break stmt can only occur inside a loop (for/while)
+        if environment.in_scope('for') or environment.in_scope('while'):
+            return log_type_error(
+                'break outside loop', environment.filename, self.meta
+            )
+
+        return Type()
+
+    def codegen(self, indentation_level: int = 0) -> str:
+        """Return the Python3 equivelent to a break stmt."""
+        return '{indent}break'.format(indent=indent(indentation_level))
