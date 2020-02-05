5ad512f4cf8ab1a89b7674fa113c3797ba583b12
Brandon Harris <b.harris.4@warwick.ac.uk>
2020-02-05 23:58:49 +0000

    Added type check and codegen for method

--- typt/class_method_node.py
+++ typt/class_method_node.py
@@ -1,9 +1,9 @@
 """class_method_node.py - holds the AST node for a class method."""
 
 from typt.func_def_node import FuncDefNode
-from typt.func_signature_node import FuncSignatureNode
 
-from typt.suite_node import SuiteNode
+from typt.typt_types import Type
+from typt.environment import Environment
 
 
 class ClassMethodNode(FuncDefNode):
@@ -12,3 +12,11 @@ class ClassMethodNode(FuncDefNode):
     def __init__(self, *args, **kwargs):
         """Specialisation of function definition."""
         super().__init__(*args, **kwargs)
+
+    def check_type(self, environment: Environment) -> Type:
+        """Check method type is valid."""
+        return super().check_type(environment)
+
+    def codegen(self, indentation_level: int = 0) -> str:
+        """Return Python3 equivalent code."""
+        return super().codegen(indentation_level)
