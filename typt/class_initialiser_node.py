89ad90124916f3c96f8796353592dd57f6b048e2
Brandon Harris <b.harris.4@warwick.ac.uk>
2020-02-05 23:58:41 +0000

    Added type check and codegen for initialiser

--- typt/class_initialiser_node.py
+++ typt/class_initialiser_node.py
@@ -1,11 +1,11 @@
 """class_initialiser_node.py - holds the AST node for a class initialiser."""
 
+from typt.suite_node import SuiteNode
 from typt.func_def_node import FuncDefNode
 from typt.func_signature_node import FuncSignatureNode
 
-from typt.suite_node import SuiteNode
-
-from typt.typt_types import NoneType, NameTypePair
+from typt.environment import Environment
+from typt.typt_types import NameTypePair, Type, NoneType
 
 from typing import List
 
@@ -13,10 +13,18 @@ from typing import List
 class ClassInitialiserNode(FuncDefNode):
     """ClassInitialiserNode AST node."""
 
-    def __init__(self, parameters: List[NameTypePair], suite: SuiteNode):
+    def __init__(self, parameters: List[NameTypePair], suite: SuiteNode, *args, **kwargs):
         """Create as FunDefNode with fixed name."""
         # Create signature with return type and add parameters
-        func_signnature = FuncSignatureNode('self', NoneType())
+        func_signnature = FuncSignatureNode('__init__', NoneType())
         func_signnature.parameter_list = parameters
 
-        super().__init__(func_signnature, suite)
+        super().__init__(func_signnature, suite, *args, **kwargs)
+
+    def check_type(self, environment: Environment) -> Type:
+        """Check initialiser type is valid."""
+        return super().check_type(environment)
+
+    def codegen(self, indentation_level: int = 0) -> str:
+        """Return Python3 equivalent code."""
+        return super().codegen(indentation_level)
