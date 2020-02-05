6f41999f66f2e70af6aab1cdec2876b703d2ef23
Brandon Harris <b.harris.4@warwick.ac.uk>
2020-02-05 21:40:40 +0000

    Type check for function

--- typt/func_def_node.py
+++ typt/func_def_node.py
@@ -2,6 +2,10 @@
 
 from typt.node import Node
 
+from typt.codegen import indent
+from typt.environment import Environment
+from typt.typt_types import Type, InvalidType, is_invalid_type
+
 from typt.suite_node import SuiteNode
 from typt.func_signature_node import FuncSignatureNode
 
@@ -15,3 +19,20 @@ class FuncDefNode(Node):
         self.suite = suite
 
         super().__init__(*args, **kwargs)
+
+    def check_type(self, environment: Environment) -> Type:
+        """Check type for the function body."""
+        # RULE Function signature is valid
+        # RULE Suite is valid
+
+        # RULE 1
+        func = is_invalid_type(self.func_signature.check_type(environment))
+
+        # RULE 2
+        suite = is_invalid_type(self.suite.check_type(environment))
+
+        return InvalidType() if any([func, suite]) else Type()
+
+    def codegen(self, indentation_level: int = 0) -> str:
+        """Return Python3 equivalent."""
+        pass
