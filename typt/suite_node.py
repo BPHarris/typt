8cec922229cff6cf7f9f009ef34d9dda6d1c6eb0
Brandon Harris <b.harris.4@warwick.ac.uk>
2020-02-05 20:22:44 +0000

    Added type checking to suite

--- typt/suite_node.py
+++ typt/suite_node.py
@@ -2,6 +2,10 @@
 
 from typt.node import Node
 
+from typt.codegen import indent
+from typt.environment import Environment
+from typt.typt_types import Type, InvalidType, is_invalid_type, log_type_error
+
 
 class SuiteNode(Node):
     """SuiteNode AST node."""
@@ -11,3 +15,20 @@ class SuiteNode(Node):
         self.stmt_list = list()
 
         super().__init__(*args, **kwargs)
+
+    def check_type(self, environment: Environment) -> Type:
+        """Perfrom type checking for this suite."""
+        # RULE All stmts are valid
+
+        suite_env = environment.add_child('suite')
+
+        # RULE 1
+        stmts_invalid = list()
+        for stmt in self.stmt_list:
+            stmts_invalid += [is_invalid_type(stmt.check_type(suite_env))]
+
+        return InvalidType() if any(stmts_invalid) else Type()
+
+    def codegen(self, indentation_level: int = 0) -> str:
+        """Return the Python3 equivalent for a suite."""
+        pass
