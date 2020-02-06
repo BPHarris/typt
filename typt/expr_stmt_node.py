7c4396fb3d962b8396e6451d64d295c580d1c047
Brandon Harris <b.harris.4@warwick.ac.uk>
2020-02-06 19:38:40 +0000

    Added type check and codegen to expr stmt node

--- typt/expr_stmt_node.py
+++ typt/expr_stmt_node.py
@@ -3,12 +3,24 @@
 from typt.stmt_node import StmtNode
 from typt.test_node import TestNode
 
+from typt.codegen import indent
+from typt.typt_types import Type
+from typt.environment import Environment
+
 
 class ExprStmtNode(StmtNode):
     """ExprStmtNode AST node."""
 
-    def __init__(self, test_node: TestNode, depth: int = 0):
+    def __init__(self, test_node: TestNode, *args, **kwargs):
         """Set the test node (expression body)."""
         self.test_node = test_node
 
-        super().__init__(depth=depth)
+        super().__init__(*args, **kwargs)
+
+    def check_type(self, environment: Environment) -> Type:
+        """Return the type of the contained expression."""
+        return self.test_node.check_type(environment)
+
+    def codegen(self, indentation_level: int = 0) -> str:
+        """Return the Python3 code of thi given expression."""
+        return f'{indent(indentation_level)}{self.test_node.codegen()}'
