832daa30acee7391cd85651d9807c144396904c9
Brandon Harris <b.harris.4@warwick.ac.uk>
2020-02-19 23:10:56 +0000

    Codegen for atom expr

--- typt/test/atom_expr_node.py
+++ typt/test/atom_expr_node.py
@@ -6,6 +6,9 @@ from typt.test.atom_node import AtomNode
 from typt.argument_list_node import ArgumentListNode
 from typt.subscript_node import SubscriptNode
 
+from typt.codegen import indent
+from typt.environment import Environment
+
 from typing import Union, List
 
 
@@ -24,3 +27,28 @@ class AtomExprNode(TestNode):
         """Set atom and initial (empty) trailer_list."""
         self.atom = atom                # type: AtomNode
         self.trailer_list = list()      # type: List[TrailerType]
+
+    def check_type(self, environment: Environment) -> Type:
+        """Check the type of an atom expression."""
+        pass
+
+    def codegen(self, indentation_level: int = 0) -> str:
+        """Codegen for the atom expression."""
+        indentation = indent(indentation_level)
+
+        output = f'{indentation}{self.atom.codegen()}'
+
+        for trailer in self.trailer_list:
+            # atom.trailer
+            if isinstance(trailer, str):
+                output += f'.{trailer}'
+
+            # atom[trailer]
+            if isinstance(trailer, SubscriptNode):
+                output += f'[{trailer.codegen()}]'
+
+            # atom(trailer)
+            if isinstance(trailer, ArgumentListNode):
+                output += f'({trailer.codegen()})'
+
+        return output
