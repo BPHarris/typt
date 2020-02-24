fe81a5d013d48b1eca4a8eec193a770c419c679a
Brandon Harris <b.harris.4@warwick.ac.uk>
2020-02-24 14:58:38 +0000

    Added missing arg_list codegen

--- typt/argument_list_node.py
+++ typt/argument_list_node.py
@@ -2,6 +2,10 @@
 
 from typt.node import Node
 
+from typt.codegen import indent
+from typt.typt_types import Type
+from typt.environment import Environment
+
 
 class ArgumentListNode(Node):
     """ArgumentListNode AST node.
@@ -14,3 +18,11 @@ class ArgumentListNode(Node):
     def __init__(self):
         """Initialise argument_list."""
         self.argument_list = list()
+
+    def check_type(self, environment: Environment) -> Type:
+        """Delegate to super (raise error)."""
+        return super().check_type()
+
+    def codegen(self, indentation_level: int = 0) -> str:
+        """Generate code for argument list."""
+        return ', '.join([a.codegen() for a in self.argument_list])
