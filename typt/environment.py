5446c8909287dc8fb1d9765c27f20acf5aaca24a
Brandon Harris <b.harris.4@warwick.ac.uk>
2020-03-03 21:18:52 +0000

    Added ability to print environment in JSON format

--- typt/environment.py
+++ typt/environment.py
@@ -191,7 +191,24 @@ class Environment:
 
     def __repr__(self) -> str:
         """Return string representation of the dictionary."""
-        contents_str = '\n'.join(
-            ['\t' + repr(k) + ' : ' + repr(v) for k, v in self.environment.items()]
-        )
-        return self.filename + '\n' + contents_str
+        to_show = {
+            'name': self.name,
+            'scope': self.scope,
+            'contents': self.environment
+        }
+
+        return f'Environment{{{to_show}}}'
+
+
+def json_encode_environment(e) -> Dict:
+    """Return a JSON-encodable dict representation of the given environment."""
+    if isinstance(e, Environment):
+        return {
+            'name': e.name,
+            'scope': e.scope,
+            'contents': e.environment
+        }
+    elif isinstance(e, Type):
+        return repr(e)
+    else:
+        raise TypeError(f'Can\'t encode {e.__class__.__name__}')
