aebe8c04c2c229ae0167263da9c91e7094bd3bd0
Brandon Harris <b.harris.4@warwick.ac.uk>
2020-03-04 21:59:40 +0000

    Created initial Typt stdlib

---
+++ typt_stdlib.py
@@ -0,0 +1,17 @@
+"""typt_std_lib.py -- implements the typt standard library."""
+
+
+#### Printing Functions ####
+print_none = print_bool = print_int = print_float = print_string = print
+
+#### Casting Functions: *_to_bool ####
+none_to_bool = int_to_bool = float_to_bool = string_to_bool = bool
+
+#### Casting Functions: *_to_int ####
+none_to_int = bool_to_int = float_to_int = string_to_int = int
+
+#### Casting Functions: *_to_float ####
+none_to_float = bool_to_float = int_to_float = string_to_float = float
+
+#### Casting Functions: *_to_str ####
+none_to_string = bool_to_string = int_to_string = float_to_string = string
