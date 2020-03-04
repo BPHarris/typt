f5bc6f127e840f5f068639e675c0e2016107df24
Brandon Harris <b.harris.4@warwick.ac.uk>
2020-03-04 22:01:51 +0000

    Fixed *_to_string error in stdlib

--- typt_stdlib.py
+++ typt_stdlib.py
@@ -14,4 +14,4 @@ none_to_int = bool_to_int = float_to_int = string_to_int = int
 none_to_float = bool_to_float = int_to_float = string_to_float = float
 
 #### Casting Functions: *_to_str ####
-none_to_string = bool_to_string = int_to_string = float_to_string = string
+none_to_string = bool_to_string = int_to_string = float_to_string = str
