90a5c94330ce0fdbb8bac96fe720a729f007b565
Brandon Harris <b.harris.4@warwick.ac.uk>
2020-03-07 21:49:01 +0000

    Updated

--- presentation/money_example.py
+++ presentation/money_example.py
@@ -2,11 +2,6 @@ from money import Money
 from decimal import Decimal
 
 
-def get_check_amount():
-    """Return dummy user input for check amount, in this case £3.75."""
-    return 3.75
-
-
 def main():
     user_balance = Money(0.0, 'GBP')
 
@@ -15,7 +10,7 @@ def main():
     print('Your balance:', user_balance.amount)
 
     # The user cashes a cheque and display new balance
-    user_balance.amount = user_balance.amount + get_check_amount()
+    user_balance.amount = user_balance.amount + 3.75
     print('Cheque cashed, new balance:', user_balance.amount)
 
 
